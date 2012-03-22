dba23ffc5aa0efeb6bbd1ef03e6fcd6ec2897ba6
David Schwertfeger <david.schwertfeger@googlemail.com>
2012-03-22 22:00:36 +0100

    completed exercise3 and added TomSawyer.txt to the 'files' folder

--- src/dsc/sheet1/exercise3.java
+++ src/dsc/sheet1/exercise3.java
@@ -1,5 +1,7 @@
 package dsc.sheet1;
 
+import StdLib.*;
+
 public class exercise3 {
 
 	/**
@@ -19,8 +21,23 @@ public class exercise3 {
 
 	public static void main(String[] args) {
 		
+		char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		
+		int[] counter = new int[alphabet.length];
 		
+		String text = StdIn.readAll();
+		
+		for(int i = 0; i < text.length(); i++) {
+			for(int j = 0; j < alphabet.length; j++) {
+				if (text.charAt(i) == alphabet[j]) {
+					counter[j]++;
+				}
+			}
+		}
+		
+		for(int i = 0; i < alphabet.length; i++) {
+			StdOut.println(alphabet[i] + " = " + counter[i]);
+		}
 	}
-
 }
