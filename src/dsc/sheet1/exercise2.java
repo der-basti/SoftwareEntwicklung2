7c3c2676f26f0bc5e92598a39eef088e44d409a9
David Schwertfeger <david.schwertfeger@googlemail.com>
2012-03-22 15:12:50 +0100

    finished exercise 2

--- src/dsc/sheet1/exercise2.java
+++ src/dsc/sheet1/exercise2.java
@@ -1,13 +1,37 @@
 package dsc.sheet1;
 
+import java.awt.Color;
+import StdLib.Picture;
+
 public class exercise2 {
 
 	/**
 	 * @param args
+	 * 
+	 * 
+	 *            Schreiben Sie ein Programm, welches den Inhalt einer per
+	 *            Kommandozeilen- argument angegebenen Bilddatei auf dem
+	 *            Bildschirm anzeigt, anschlie§end vertikal spiegelt und das
+	 *            Ergebnis ebenfalls darstellt.
+	 * 
 	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Picture pic = new Picture(args[0]);
+		pic.show();
+
+		int width = pic.width();
+		int height = pic.height();
+		
+		Picture mirror = new Picture(width, height);
+		
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				
+				mirror.set(width - x - 1, y, pic.get(x, y));
+			}
+		}
+		mirror.show();
 	}
-
 }
---
+++ src/dsc/sheet1/exercise3.java
@@ -0,0 +1,26 @@
+package dsc.sheet1;
+
+public class exercise3 {
+
+	/**
+	 * 
+	 * Erstellen Sie ein Programm, welches einen Text (z.B. TomSawyer.txt) von
+	 * der Standardeingabe einliest, den prozentualen Anteil des Auftauchens der
+	 * einzelnen Kleinbuchstaben im Text berechnet und auf dem Bildschirm
+	 * anzeigt.
+	 * 
+	 * Ignorieren Sie Gro§buchstaben, Satzzeichen, Leerzeichen usw. in Ihrer
+	 * Analyse.
+	 * 
+	 * @param args
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
