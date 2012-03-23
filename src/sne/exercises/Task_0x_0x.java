2f9c46d5997664fad23f6ed59c077ceeb454dc23
der-basti <devnull@der-basti.com>
2012-03-23 14:49:46 +0100

    - update eclipse project settings - sheet1 task 2 & 3

---
+++ src/sne/exercises/Task_0x_0x.java
@@ -0,0 +1,13 @@
+package sne.exercises;
+
+/**
+ * FIXME [sne] Exercise sheet: 0x task: 0x <br />
+ * Description:<br />
+ * ...
+ */
+public class Task_0x_0x {
+
+	public static void main(String[] args) {
+
+	}
+}
