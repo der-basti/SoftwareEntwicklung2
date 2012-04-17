17fc85d9d28af196d39dc0d174594d59efa712f0
der-basti <devnull@der-basti.com>
2012-04-17 15:08:55 +0200

    sheet 5
    -init sheet 5
    -foo

---
+++ src/sne/exercise/sheet5/SpecialItem.java
@@ -0,0 +1,55 @@
+package sne.exercise.sheet5;
+
+class SpecialItem extends Product {
+
+	private double discount;
+
+	public SpecialItem(String productId, String description, double cost,
+			int storageTime) {
+
+		super(productId, description, cost, storageTime);
+		if (super.getStorageTime() > 12) {
+			this.discount = 0.30;
+		} else {
+			this.discount = 0.10;
+		}
+	}
+
+	public SpecialItem(String productId, String description, double cost,
+			int storageTime, double discount) {
+		super(productId, description, cost, storageTime);
+		this.discount = discount;
+	}
+
+	@Override
+	public int calcRetailPrice() {
+		return new Double(super.calcRetailPrice() * (1 - this.discount))
+				.intValue();
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder("Sonderposten:");
+		sb.append(System.getProperty("line.separator"))
+				.append(super.getProductId()).append(" ");
+		sb.append("\"").append(super.getDescription()).append("\" ");
+		sb.append("cost:").append(super.getCost()).append("€ ");
+		sb.append("retail price:").append(this.calcRetailPrice()).append("€ ");
+		sb.append("storage time:").append(super.getStorageTime())
+				.append(" months");
+		sb.append(System.getProperty("line.separator")).append("(");
+		sb.append("old retail price:").append(super.calcRetailPrice())
+				.append("€ ");
+		sb.append("discount:").append(this.discount * 100).append("%");
+		return sb.append(")").toString();
+	}
+
+	public double getDiscount() {
+		return this.discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+}
